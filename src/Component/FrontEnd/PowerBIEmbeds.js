b7c2957042a28ffca17da94a344f3bb834f24e8f
sandeshi <nikkamsandeshi341@gmail.com>
2024-08-30 00:48:58 +0530

    Incommit2

--- src/Component/FrontEnd/PowerBIEmbeds.js
+++ src/Component/FrontEnd/PowerBIEmbeds.js
@@ -1,23 +1,24 @@
 // PowerBIEmbed.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { PowerBIEmbed } from 'powerbi-client-react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { PowerBIEmbed } from "powerbi-client-react";
 
 const PowerBIEmbeds = () => {
-  const [embedToken, setEmbedToken] = useState('');
-  const [embedUrl, setEmbedUrl] = useState('https://app.powerbi.com/groups/71488eaa-1122-43e1-9370-3a3148dd6128/reports/24e426e7-0d0e-465b-85cb-e7dcded17897/33644b731bee20050da9?experience=power-bi'
-);
-  const reportId = "24e426e7-0d0e-465b-85cb-e7dcded17897"
-; // Replace with your actual report ID
-
+  const [embedToken, setEmbedToken] = useState("");
+  const [embedUrl, setEmbedUrl] = useState(
+    "https://app.powerbi.com/groups/71488eaa-1122-43e1-9370-3a3148dd6128/reports/24e426e7-0d0e-465b-85cb-e7dcded17897/33644b731bee20050da9?experience=power-bi"
+  );
+  const reportId = "24e426e7-0d0e-465b-85cb-e7dcded17897"; // Replace with your actual report ID
   useEffect(() => {
     const fetchembedToken = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:5000/get_embed_token');
+        const response = await axios.get(
+          "http://127.0.0.1:5000/get_embed_token"
+        );
         setEmbedToken(response.data.token);
         setEmbedUrl(response.data.embedUrl); // Assuming the API returns both token and URL
       } catch (error) {
-        console.error('Error fetching embed token:', error);
+        console.error("Error fetching embed token:", error);
       }
     };
 
@@ -25,18 +26,18 @@ const PowerBIEmbeds = () => {
   }, []);
 
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: "100vh", width: "100%" }}>
       {embedToken && embedUrl && (
         <PowerBIEmbed
           embedConfig={{
-            type: 'report',
+            type: "report",
             id: reportId,
             embedUrl: embedUrl,
             accessToken: embedToken,
             settings: {
               filterPaneEnabled: true,
-              navContentPaneEnabled: true
-            }
+              navContentPaneEnabled: true,
+            },
           }}
           cssClassName="powerbi-report"
         />
